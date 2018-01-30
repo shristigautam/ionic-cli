4f8dfe441b9d7b94477ac2b3b2239f55e78b08f1
mhartington <mikehartington@gmail.com>
2018-01-30 11:48:55 -0500

    working on schematics

--- packages/@ionic/schematics-angular/page/files/__path__/__name@kebabCase__/__name@kebabCase__.page.ts
+++ packages/@ionic/schematics-angular/page/files/__path__/__name@kebabCase__/__name@kebabCase__.page.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-    selector: '<%= kebabCase(name) %>-page',
-    templateUrl: './<%= kebabCase(name) %>.page.html',
-    styleUrls: ['./<%= kebabCase(name) %>.page.<%= styleext %>'],
+  selector: '<%= kebabCase(name) %>-page',
+  templateUrl: './<%= kebabCase(name) %>.page.html',
+  styleUrls: ['./<%= kebabCase(name) %>.page.<%= styleext %>'],
 })
 export class <%= upperFirst(camelCase(name)) %>Page implements OnInit {
 
